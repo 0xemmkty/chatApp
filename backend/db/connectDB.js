1fd39ed4f9f5f66e17bf0d336d7674ac7f4dbc6b
pz <2733224120@qq.com>
2023-12-23 20:36:43 -0600

    111

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -5,7 +5,8 @@ require("dotenv").config({path : "backend/utills/.env"});
 
 function connectDB() {
   mongoose
-    .connect(process.env.DB_LINK)
+    .connect('mongodb+srv://2733224120:Aa1051520520@cluster0.ynv1fdo.mongodb.net/chatApp?retryWrites=true&w=majority')
+    
     .then(() => console.log("DB_CONNECTED"))
     .catch((err) => console.log(`err ${err.message}`));
 }
